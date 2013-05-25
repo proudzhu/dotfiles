c038a60aeb52176252095e7623b0fec4bed2db64
proudzhu <proudzhu.fdu@gmail.com>
2013-05-25 10:53:49 +0800

    deleted:    Scripts/alias 	deleted:    Scripts/aurcomments 	deleted:    Scripts/aurdiff 	deleted:    Scripts/aurebuildcheck 	deleted:    Scripts/aurebuildcheck2 	deleted:    Scripts/aurprovidecheck 	deleted:    Scripts/backup.sh 	deleted:    Scripts/mycheckupdates 	deleted:    Scripts/pacman-disowned 	deleted:    Scripts/profile-cleaner.sh 	deleted:    Scripts/qname 	deleted:    Scripts/tputcolors 	deleted:    Scripts/whoneeds 	deleted:    Scripts/ydcv 	modified:   .zsh/z (modified content) 	modified:   .zsh/zsh-history-substring-search (modified content) 	modified:   .zsh/zsh-syntax-highlighting (modified content)

--- Scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-START=$(date +%s)
-rsync -aAXv /* $1 --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found/*,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/chromium/*,/home/*/.mozilla/firefox/*.default/Cache/*,/home/*/.cpan/*,/home/*/abs/*,/var/lib/pacman/sync/*,/var/abs/*,/var/cache/*,/var/log/*,/var/tmp/*}
-FINISH=$(date +%s)
-echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
-
-touch $1/"Backup from $(date '+%A, %d %B %Y, %T')"
